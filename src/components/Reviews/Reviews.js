6812924e736dc69ab618d895ebaba409fc37c59e
Rabiul Islam <rabiul.dev@gmail.com>
2022-05-30 17:44:23 +0600

    Reviews component completed

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import useReviews from '../../hooks/useReviews'
+import Review from '../Review/Review'
 import "./Reviews.css"
 const Reviews = () => {
+  const [reviews] = useReviews()
   return (
-    <div>Reviews</div>
+    <div className="reviews-container">
+      <h1 className='review-title'>All Customer Reviews</h1>
+      <div className="reviews">
+      
+        {reviews.map(revi => <Review key={revi.id} revi={revi} />)}
+      </div>
+    </div>
   )
 }
 
