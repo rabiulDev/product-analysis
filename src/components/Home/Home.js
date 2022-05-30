95f887781a7516debb4eea0d5de722a6a0023c7a
Rabiul Islam <rabiul.dev@gmail.com>
2022-05-30 16:33:13 +0600

    hero section added to home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import mackbook from "../../assets/hero-image/mackbook.png"
 import "./Home.css"
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <main className='main-container'>
+            <div className="hero">
+                <div className="hero-info">
+                    <h1>The best Laptop on the planet</h1>
+                    <h1 className='color-purple'>Get your Laptop by order now</h1>
+                    <p>
+                        The most powerfull laptop for complete your any task.
+                        You can use it in your daily work. You can do graphics design, video editing,
+                        gaming, coding, programming etc on this laptop very smoothly. So don't late to get it ORDER now.
+
+                    </p>
+                    <div className="hero-btn">
+                        <button>Live Demo</button>
+                    </div>
+                </div>
+            </div>
+            <div className="hero-img">
+                <img src={mackbook} alt="" />
+            </div>
+
+
+        </main>
+    )
 }
 
 export default Home
