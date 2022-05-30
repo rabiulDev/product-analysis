3b246dfd43f5b746201b62a17ec64c777c5946cb
Rabiul Islam <rabiul.dev@gmail.com>
2022-05-30 17:06:24 +0600

    add main-container div in home component and add css on main tag

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,27 +5,31 @@ import "./Home.css"
 const Home = () => {
     const [reviews, setReviews] = useReviews()
     return (
-        <main className='main-container'>
-            <div className="hero">
-                <div className="hero-info">
-                    <h1>The best Laptop on the planet</h1>
-                    <h1 className='color-purple'>Get your Laptop by order now</h1>
-                    <p>
-                        The most powerfull laptop for complete your any task.
-                        You can use it in your daily work. You can do graphics design, video editing,
-                        gaming, coding, programming etc on this laptop very smoothly. So don't late to get it ORDER now.
+        <main>
+            <div className='main-container'>
+                <div className="hero">
+                    <div className="hero-info">
+                        <h1>The best Laptop on the planet</h1>
+                        <h1 className='color-purple'>Get your Laptop by order now</h1>
+                        <p>
+                            The most powerfull laptop for complete your any task.
+                            You can use it in your daily work. You can do graphics design, video editing,
+                            gaming, coding, programming etc on this laptop very smoothly. So don't late to get it ORDER now.
 
-                    </p>
-                    <div className="hero-btn">
-                        <button>Live Demo</button>
+                        </p>
+                        <div className="hero-btn">
+                            <button>Live Demo</button>
+                        </div>
                     </div>
                 </div>
+                <div className="hero-img">
+                    <img src={mackbook} alt="" />
+                </div>
             </div>
-            <div className="hero-img">
-                <img src={mackbook} alt="" />
-            </div>
+            <article>
 
-           {reviews.length}
+            </article>
+
         </main>
     )
 }
